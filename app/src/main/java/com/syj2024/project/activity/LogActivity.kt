25e5215860a09fd28a8af18d7b9975b48a8c786b
yeonjuss <syeonju01@gmail.com>
2024-09-15 13:50:24 +0900

    5 commit

---
+++ app/src/main/java/com/syj2024/project/activity/LogActivity.kt
@@ -0,0 +1,17 @@
+package com.syj2024.project.activity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.syj2024.project.databinding.ActivityLogBinding
+
+
+class LogActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityLogBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+    }
+    
+}
--- app/src/main/java/com/syj2024/project/activity/MainActivity.kt
+++ app/src/main/java/com/syj2024/project/activity/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.syj2024.project.activity
 
-import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -11,10 +9,11 @@ import com.prolificinteractive.materialcalendarview.CalendarDay
 import com.prolificinteractive.materialcalendarview.DayViewDecorator
 import com.prolificinteractive.materialcalendarview.DayViewFacade
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView
-import com.prolificinteractive.materialcalendarview.OnDateSelectedListener
 import com.prolificinteractive.materialcalendarview.spans.DotSpan
 import com.syj2024.project.R
 import com.syj2024.project.databinding.ActivityMainBinding
+import com.syj2024.project.fragment.CompetitonListFragment
+import com.syj2024.project.fragment.NearbyGymFragment
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
@@ -29,11 +28,15 @@ class MainActivity : AppCompatActivity() {
         calendarView.setSelectionColor(ContextCompat.getColor(this, R.color.select))
 
         // 날짜 선택 리스너 설정
-        calendarView.setOnDateChangedListener { widget, date, selected ->
-            // 날짜 선택 시 실행될 동작
-            Toast.makeText(this, "Selected Date: ${date.day}/${date.month}/${date.year}",Toast.LENGTH_SHORT).show()
+        calendarView.setOnDateChangedListener{ widget, date, selected ->
 
-        class EventDecorator(private val color: Int, private val dates: HashSet<CalendarDay>) :
+
+            // 날짜 선택 시 실행될 동작
+            Toast.makeText(this, "Selected Date: ${date.day}/${date.month+1}/${date.year}",Toast.LENGTH_SHORT).show()
+
+
+
+            class EventDecorator(private val color: Int, private val dates: HashSet<CalendarDay>) :
             DayViewDecorator {
 
             override fun shouldDecorate(day: CalendarDay?): Boolean {
@@ -45,15 +48,36 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        calendarView.state().edit()
-            .setMinimumDate(CalendarDay.from(2022, 1, 1)) // 최소 날짜 설정
-            .setMaximumDate(CalendarDay.from(2025, 12, 31)) // 최대 날짜 설정
-            .commit()
+            calendarView.state().edit()
+                .setMinimumDate(CalendarDay.from(2022, 1, 1)) // 최소 날짜 설정
+                .setMaximumDate(CalendarDay.from(2025, 12, 31)) // 최대 날짜 설정
+                .commit()
 
 
     }
 
+        binding.fab.setOnClickListener {
+            startActivity(Intent(this, LogActivity::class.java))
+
+        }
+
+
+        binding.bnv.setOnItemSelectedListener { menuItem ->
+            // 선택된 탭에 따라 반응
+            when(menuItem.itemId) {
+                R.id.bnv_rc -> supportFragmentManager.beginTransaction().replace(R.id.fragment_container1, CompetitonListFragment()).commit()
+                R.id.bnv_sd -> supportFragmentManager.beginTransaction().replace(R.id.fragment_container1, CompetitonListFragment()).commit()
+                R.id.bnv_search -> supportFragmentManager.beginTransaction().replace(R.id.fragment_container1, NearbyGymFragment()).commit()
+
+            }
+
+            // 리턴값을 true로 해야 선택변경에 따른 UI가 반영됨
+            true
+        }
+
+
     }// onCreate
+
 } //MainActivity //
 
 
