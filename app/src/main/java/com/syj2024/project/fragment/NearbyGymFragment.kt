f4fdb6e16578cb5318deffa54b13dff03d04b660
yeonjuss <syeonju01@gmail.com>
2024-09-18 22:17:42 +0900

    5 commit

---
+++ app/src/main/java/com/syj2024/project/fragment/NearbyGymFragment.kt
@@ -0,0 +1,24 @@
+package com.syj2024.project.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.syj2024.project.R
+import com.syj2024.project.databinding.FragmentCompetitonListBinding
+import com.syj2024.project.databinding.FragmentNearbyGymBinding
+
+class NearbyGymFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding= FragmentNearbyGymBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    lateinit var binding: FragmentNearbyGymBinding
+
+}
